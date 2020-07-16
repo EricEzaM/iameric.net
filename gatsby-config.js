04b55a296c10b2e95eddac43125f5d740a5c8a96
Eric M <itsjusteza@gmail.com>
2020-07-16 20:35:21 +1000

    Basic config setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,49 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `IAmEric`,
+    description: `Programming n stuff`,
+    author: `Eric Michaels`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    // Sass/Scss support
+    `gatsby-plugin-sass`,
+    // File system sources
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/content/assets`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: `articles`,
+        path: `${__dirname}/content/articles`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
+      },
+    },
+    // Images
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    // Markdown related
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+          },
+          "gatsby-remark-prismjs",
+        ],
+      },
+    },
   ],
 }
