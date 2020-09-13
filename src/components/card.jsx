871723fa622aecf1c397824923f99942aceca8b9
Eric M <itsjusteza@gmail.com>
2020-09-13 18:57:32 +1000

    Remove debugger call

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import TagList from "./tag-list"
 
 const Card = ({ link, title, body, tags, imgSrc, className, ...props }) => {
-  debugger
   return (
     <div {...props} className={["card", className].join(" ")}>
       <div className={"card__content"}>
