1eb87f2c66da981261547390ef45a8c44ae320c1
EricEzaM <itsjusteza@gmail.com>
2020-12-25 21:32:15 +1000

    Added category display to snippet page

--- src/templates/snippet.jsx
+++ src/templates/snippet.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import TagList from "../components/tag-list"
 
 const Snippet = ({ data }) => {
   const post = data.snippet
@@ -11,6 +12,7 @@ const Snippet = ({ data }) => {
         <div className="article__header">
           <h1>{post.frontmatter.title}</h1>
           <div className="article__meta">
+            <TagList tags={[post.frontmatter.category]} />
             <time>Published {post.frontmatter.date}</time>
           </div>
         </div>
