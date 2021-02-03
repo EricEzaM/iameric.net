912cafecae714c2bfd8665654fe402e96999678a
Eric M <itsjusteza@gmail.com>
2021-02-03 22:46:51 +1000

    Added about page.
    Fixed display of anchor tags which appear in main body text.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,15 @@ const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" />
-      <div>Hello!</div>
+      <h3 className="subtitle">
+        A mechanical engineer who loves programming.
+      </h3>
+      <p>
+        This site is a place for me to document some of the things I do - for myself to reference and look back on in the future, but also for others too if they find it useful.
+      </p>
+      <p>
+        I'm based in Australia. If you would like to contact me, feel free to send me an email at <a href="mailto:eric@iameric.net">eric@iameric.net</a>.
+      </p>
     </Layout>
   )
 }
