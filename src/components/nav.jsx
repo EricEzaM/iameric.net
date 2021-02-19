2d0364af9c3aa2a3e200fd39438e48ab16f5f38d
Eric M <itsjusteza@gmail.com>
2021-02-19 16:47:37 +1000

    Update nav menu styling on mobile to be a show/hide menu.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+
+import CaretRightIcon from "../images/svg/caret-right.svg";
+import CaretDownIcon from "../images/svg/caret-down.svg";
 
 const Nav = () => {
   const headerImage = useStaticQuery(graphql`
@@ -15,6 +18,8 @@ const Nav = () => {
     }
   `)
 
+  const [menuShown, setMenuShown] = useState(false)
+  
   return (
     <nav className="nav">
       <Link to="/">
@@ -26,7 +31,13 @@ const Nav = () => {
         />
       </Link>
 
-      <ul className="nav__list">
+      <button className="nav__title" onClick={e => setMenuShown(!menuShown)}>
+        {/* Offset the icon by 24 px so the "menu" still gets centered*/}
+        {!menuShown && <CaretRightIcon style={{marginLeft:"-24px"}}/>}
+        { menuShown && <CaretDownIcon style={{marginLeft:"-24px"}}/>}
+        Menu
+      </button>
+      <ul className={`nav__list ${menuShown ? "nav__list--active" : ""}`}>
         {["Articles", "Snippets", "Projects", "About"].map(e => (
           <li key={e.toLowerCase()} className="nav__item">
             <Link activeClassName="is-active" to={`/${e.toLowerCase()}`}>
